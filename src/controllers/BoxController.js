d42e1488678b4b5a589787b0f4f2e4069626248d
Matheus Crivellari <matheus.acrivellari@gmail.com>
2019-04-09 13:36:15 -0300

    feat: basic Box cration done

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -3,7 +3,10 @@ const Box = require('../model/Box');
 // Class which will control the Box model
 class BoxController{
 	async store(req, res){
-		const box = await Box.create({ title : 'Rocketseat'});
+		// const box = await Box.create({ title : req.body.title });
+		
+		// This line and the above are the same in this case
+		const box = await Box.create(req.body);
 		return res.json(box);
 	}
 }
