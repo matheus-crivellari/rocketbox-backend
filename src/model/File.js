6597ae1a74e4f66e5e0fc9c542270711e9b6272e
Matheus Crivellari <matheus.acrivellari@gmail.com>
2019-04-10 01:00:04 -0300

    feat: added route for static files

--- src/model/File.js
+++ src/model/File.js
@@ -11,13 +11,15 @@ const File = new mongoose.Schema({
 	},
 },{
 	timestamps : true, // Adds createdAt and updatedAt to schema
-	toObject: { virtuals: true },
-	toJSON: { virtuals: true },
+	toObject: { virtuals: true }, // Consider virtuals when casting to object
+	toJSON: { virtuals: true }, // Consider virtuals when casting to JSON
 });
 
-File.virtual('url').get(()=>{
+// Here it can't be an arrow function!!
+File.virtual('url').get(function(){
+	console.log('teco', this.path);
 	return `http://localhost:3333/files/${encodeURIComponent(this.path)}`;
-})
+});
 
 // Exports model as a module for later use in express
 module.exports = mongoose.model('File', File);
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express   = require('express'),
+	  path  	= require('path'),
+ 	  mongoose  = require('mongoose');
 
 const PORT = 3333,
 	   app = express();
-	   
+
 // Connect to database
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-fqd3x.mongodb.net/omnistack?retryWrites=true', {
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-fqd3x.mongodb.net/omnistack?retryWrites=true',
+{
 	useNewUrlParser : true
 });
 
@@ -15,8 +17,12 @@ app.use(express.json());
 // Useful for file uploading
 app.use(express.urlencoded({extended:true}));
 
+// Add static routing for files
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
 // Use our routing filfe (./ relative to current file)
 app.use(require('./routes'));
+
 
 // Listen to port 3333
 app.listen(PORT);
