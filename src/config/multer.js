2e9a25868a156d9dc1400bcda2d8e9ce51b93274
Matheus Crivellari <matheus.acrivellari@gmail.com>
2019-04-09 23:42:00 -0300

    docs: improved multer comments

--- src/config/multer.js
+++ src/config/multer.js
@@ -6,11 +6,13 @@ module.exports = {
     // A smarter way to configure relative path for folders
     dest : path.resolve(__dirname, '..', '..', 'tmp'),
     storage : multer.diskStorage({
+        // Configure destination for uploads
         destination : (req, file, cbck) => {
             // Firts argument: error, second: path
             cbck(null, path.resolve(__dirname, '..', '..', 'tmp'));
         },
 
+        // Treat names before storing
         filename : (req, file, cbck) => {
             // Generate 18n bytes of random characters
             crypto.randomBytes(16, (error, hash) => {
